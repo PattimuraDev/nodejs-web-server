751d824a4d9e16ad8dddb248862c70ef5e2294e7
Dwisatriapatra <dwisatriapatra2000@gmail.com>
2022-06-13 08:27:43 +0700

    update

--- server.js
+++ server.js
@@ -1,1 +1,46 @@
-console.log('Start to learn webserver using node js');
+const http = require('http');
+
+
+const requireListener = (request, response) =>{
+    response.setHeader('Content-Type', 'text/html');
+    response.statusCode = 200;
+    
+    
+    const {method} = request;
+
+    if(method === 'GET'){
+        response.end('<h1>Ini merespon GET</h1>');
+    }
+    if(method === 'POST'){
+        let body = [];
+
+        request.on('data', (chunk) =>{
+            body.push(chunk);
+        });
+
+        request.on('end', () =>{
+            body = Buffer.concat(body).toString();
+            const{name} = JSON.parse(body);
+
+            response.end(`<h1>Hai ${name}</h1>`);
+
+        });
+
+    }
+    // if(method === 'PUT'){
+    //     response.end('<h1>Ini merespon PUT</h1>');
+    // }
+    // if(method === 'DELETE'){
+    //     response.end('<h1>Ini merespon DELETE</h1>');
+    // }
+};
+
+const server = http.createServer(requireListener);
+
+const port = 5000;
+const host = 'localhost';
+
+
+server.listen(port, host, () => {
+    console.log(`Server berjalan pada http://${host}:${port}`);
+});
